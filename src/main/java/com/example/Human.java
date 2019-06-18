e71b1e44d71930cbd9c58d34f90d65a6d34f9b53
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-18 14:49:30 +0900

    Humanクラス改装

--- src/main/java/com/example/Human.java
+++ src/main/java/com/example/Human.java
@@ -2,13 +2,13 @@ package com.example;
 
 public class Human {
 	
-	int age;
-	String name;
+	private int age;//コメントコメント
+	public String name;//コメントコメント
 	public int getAge() {
-		return age;
+		return age-10;
 	}
 	public void setAge(int age) {
-		this.age = age;
+		this.age = age+10;
 	}
 	public String getName() {
 		return name;
